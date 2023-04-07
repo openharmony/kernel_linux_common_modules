cdab0f7f60cb704b2fc5e67e0b21c985c1c5b634
liangbotong <liangbotong@huawei.com>
2023-04-07 11:31:22 +0800

    Modify related scripts and files according to the directory structure
    Signed-off-by: liangbotong <liangbotong@huawei.com>

--- newip/apply_newip.sh
+++ newip/apply_newip.sh
@@ -16,22 +16,20 @@ function main()
     pushd .
 
     cd $KERNEL_BUILD_ROOT/include/linux/
-    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/linux       $NEWIP_SOURCE_ROOT/src/linux/include/linux)/*.h ./
+    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/linux       $NEWIP_SOURCE_ROOT/src/linux-5.10/include/linux)/*.h ./
     cd $KERNEL_BUILD_ROOT/include/net/netns
-    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/net/netns   $NEWIP_SOURCE_ROOT/src/linux/include/net/netns)/*.h ./
+    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/net/netns   $NEWIP_SOURCE_ROOT/src/linux-5.10/include/net/netns)/*.h ./
     cd $KERNEL_BUILD_ROOT/include/net
-    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/net         $NEWIP_SOURCE_ROOT/src/linux/include/net)/*.h ./
+    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/net         $NEWIP_SOURCE_ROOT/src/linux-5.10/include/net)/*.h ./
     cd $KERNEL_BUILD_ROOT/include/uapi/linux
-    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/uapi/linux  $NEWIP_SOURCE_ROOT/src/linux/include/uapi/linux)/*.h ./
-    cd $KERNEL_BUILD_ROOT/include/trace/hooks
-    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/trace/hooks $NEWIP_SOURCE_ROOT/src/linux/include/trace/hooks)/*.h ./
+    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/uapi/linux  $NEWIP_SOURCE_ROOT/src/linux-5.10/include/uapi/linux)/*.h ./
 
     if [ ! -d " $KERNEL_BUILD_ROOT/net/newip" ]; then
         mkdir $KERNEL_BUILD_ROOT/net/newip
     fi
 
     cd $KERNEL_BUILD_ROOT/net/newip/
-    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/net/newip   $NEWIP_SOURCE_ROOT/src/linux/net/newip)/* ./
+    ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/net/newip   $NEWIP_SOURCE_ROOT/src/linux-5.10/net/newip)/* ./
     ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/net/newip   $NEWIP_SOURCE_ROOT/src/common)/* ./
     cd $KERNEL_BUILD_ROOT/include/uapi/linux
     ln -s -f $(realpath --relative-to=$KERNEL_BUILD_ROOT/include/uapi/linux   $NEWIP_SOURCE_ROOT/src/common)/nip_addr.h nip_addr.h
