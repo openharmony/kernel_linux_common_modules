0eb3d8cc8cc064ed1f54707d6db4e40bf217c037
liangbotong <liangbotong@huawei.com>
2023-05-15 09:56:47 +0800

    btdev code rectification
    1. The header comment contains the copyright license, feature description, author, and creation date
    2. Rectification of deep nesting of code blocks of functions
    3. Rectification of code specification problems
    4. Rectify resource leakage
    5. Rectifying reliability risks
    
    Signed-off-by: liangbotong <liangbotong@huawei.com>

--- newip/src/linux-5.10/drivers/net/bt/btdev.h
+++ newip/src/linux-5.10/drivers/net/bt/btdev.h
@@ -1,6 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ *
+ * Description: Bluetooth virtual network device used in
+ * the NewIP over Bluetooth communication scenario.
+ *
+ * Author: Yang Yanjun <yangyanjun@huawei.com>
+ *
+ * Data: 2023-03-14
  */
 
 #ifndef _BTDEV_H_
@@ -46,7 +53,9 @@
 #define STRTOLL_BASE 10
 #define BT_DEV_ID_OFFSET (sizeof(BT_DEV_PATH_PREFIX) - 1)
 #define BT_STATISTIC_KTIME_MAX ULONG_MAX
-
+#define NEWIP_TYPE_SIZE 2 /* newip type eadd */
+#define BT_MAX_MTU 65535
+#define UNKNOWN_NAME "unknown"
 /**
  * for debug
  */
@@ -111,7 +120,6 @@ struct bt_virnet {
 	struct bt_io_file *io_file;
 	struct net_device *ndev;
 	struct list_head virnet_entry;
-	struct bt_table *bt_table_head;
 	enum bt_virnet_state state;
 	struct semaphore sem;
 	wait_queue_head_t rx_queue, tx_queue;
@@ -142,13 +150,12 @@ static const char *bt_virnet_state_rep[BT_VIRNET_STAET_NUM] = {
 /**
  * inline functions
  */
-static inline int bt_get_unused_id(const u32 *bitmap)
+static inline int bt_get_unused_id(u32 bitmap)
 {
 	int i;
 
-	WARN_ON(!bitmap);
 	for (i = 0; i < BT_VIRNET_MAX_NUM + 1; ++i) {
-		if (!(*bitmap & (1 << i)))
+		if (!(bitmap & (1 << i)))
 			return i;
 	}
 	return -1; // all used
@@ -156,63 +163,73 @@ static inline int bt_get_unused_id(const u32 *bitmap)
 
 static inline void bt_set_bit(u32 *bitmap, u32 idx)
 {
-	WARN_ON(!bitmap);
-	*bitmap |= (1 << idx);
+	if (bitmap && idx <= BT_VIRNET_MAX_NUM)
+		*bitmap |= (1 << idx);
 }
 
 static inline void bt_clear_bit(u32 *bitmap, u32 idx)
 {
-	WARN_ON(!bitmap);
-	*bitmap &= ~(1 << idx);
+	if (bitmap && idx <= BT_VIRNET_MAX_NUM)
+		*bitmap &= ~(1 << idx);
 }
 
 #define SET_STATE(vn, st) bt_virnet_set_state(vn, st)
 static inline void bt_virnet_set_state(struct bt_virnet *vn,
 				       enum bt_virnet_state state)
 {
-	WARN_ON(!vn);
 	vn->state = state;
 }
 
 static inline const struct cdev *bt_virnet_get_cdev(const struct bt_virnet *vn)
 {
-	WARN_ON(!vn);
+	if (unlikely(!vn->io_file) || unlikely(!vn->io_file->bt_cdev))
+		return NULL;
+
 	return vn->io_file->bt_cdev->cdev;
 }
 
-static inline const dev_t bt_virnet_get_cdev_number(const struct bt_virnet *vn)
+static inline const int bt_virnet_get_cdev_number(const struct bt_virnet *vn, dev_t *number)
 {
-	WARN_ON(!vn);
-	return vn->io_file->bt_cdev->cdev->dev;
+	if (unlikely(!vn->io_file) || unlikely(!vn->io_file->bt_cdev) ||
+	    unlikely(!vn->io_file->bt_cdev->cdev) || unlikely(!number))
+		return -EINVAL;
+
+	*number = vn->io_file->bt_cdev->cdev->dev;
+	return OK;
 }
 
 static inline const char *bt_virnet_get_cdev_name(const struct bt_virnet *vn)
 {
-	WARN_ON(!vn);
+	if (unlikely(!vn->io_file) || unlikely(!vn->io_file->bt_cdev) ||
+	    unlikely(!vn->io_file->bt_cdev->dev_filename))
+		return UNKNOWN_NAME;
+
 	return vn->io_file->bt_cdev->dev_filename;
 }
 
 static inline struct net_device *bt_virnet_get_ndev(const struct bt_virnet *vn)
 {
-	WARN_ON(!vn);
 	return vn->ndev;
 }
 
 static inline const char *bt_virnet_get_ndev_name(const struct bt_virnet *vn)
 {
-	WARN_ON(!vn);
+	if (unlikely(!vn->ndev) || unlikely(!vn->ndev->name))
+		return UNKNOWN_NAME;
+
 	return vn->ndev->name;
 }
 
 static inline const char *bt_virnet_get_state_rep(const struct bt_virnet *vn)
 {
-	WARN_ON(!vn);
 	return bt_virnet_state_rep[vn->state];
 }
 
 static inline int bt_get_total_device(const struct bt_drv *bt_drv)
 {
-	WARN_ON(!bt_drv);
+	if (unlikely(!bt_drv->devices_table))
+		return -EINVAL;
+
 	return bt_drv->devices_table->num;
 }
 
@@ -220,7 +237,10 @@ static inline int bt_virnet_get_ring_packets(const struct bt_virnet *vn)
 {
 	int packets = 0;
 
-	WARN_ON(!vn);
+	if (unlikely(!vn->tx_ring) || unlikely(!vn->tx_ring->head) ||
+	    unlikely(!vn->tx_ring->tail))
+		return -EINVAL;
+
 	packets = vn->tx_ring->head - vn->tx_ring->tail;
 	if (unlikely(packets < 0))
 		packets += BT_RING_BUFFER_SIZE;
@@ -231,7 +251,7 @@ static inline int bt_virnet_get_ring_packets(const struct bt_virnet *vn)
 static struct bt_table *bt_table_init(void);
 static int bt_table_add_device(struct bt_table *tbl, struct bt_virnet *vn);
 static void bt_table_remove_device(struct bt_table *tbl, struct bt_virnet *vn);
-static void bt_table_delete_all(struct bt_drv *bt_drv);
+static int bt_table_delete_all(struct bt_drv *bt_drv);
 static struct bt_virnet *bt_table_find(struct bt_table *tbl, const char *ifa_name);
 static void bt_table_destroy(struct bt_drv *bt_drv);
 static void bt_delete_io_files(struct bt_drv *bt_mng);
