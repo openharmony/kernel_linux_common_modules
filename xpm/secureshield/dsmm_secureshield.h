8083d4c0533585c84b06408007fd3d702cdc584c
luyifan <842825214@qq.com>
2024-11-05 10:44:08 +0800

    Xpm: check secure shield and disable jit
    Signed-off-by: luyifan<842825214@qq.com>

---
+++ xpm/secureshield/dsmm_secureshield.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ */
+#ifndef _DSMM_SECURESHIELD_H
+#define _DSMM_SECURESHIELD_H
+
+bool dsmm_is_secureshield_enabled(void);
+
+#endif /* _DSMM_SECURESHIELD_H */
